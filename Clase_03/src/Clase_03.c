b872b1370853421d07e35d1027525e0d36906853
ernesto <ernestogigliotti@gmail.com>
2019-08-28 22:25:25 -0300

    Codigo a probar- falta test

--- Clase_03/src/Clase_03.c
+++ Clase_03/src/Clase_03.c
@@ -11,7 +11,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int status) {
-	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+#define CANT_ITERA 5
+
+int calcularMinimoMaximo(int cantidadNumeros, int *resultadoMaximo, int *resultadoMinimo);
+
+int main(void)
+{
+	int resultadoMaximo;
+	int resultadoMinimo;
+	if(calcularMinimoMaximo(CANT_ITERA,&resultadoMaximo,&resultadoMinimo)== 0)
+	{
+		printf("El resultado maximo es %d y el resultado Minimo es %d",resultadoMaximo,resultadoMinimo);
+	}
 	return 0;
 }
+
+int calcularMinimoMaximo(int cantidadNumeros, int *resultadoMaximo, int *resultadoMinimo)
+{
+	int numero;
+	int i;
+	int maximo;
+	int minimo;
+	int retorno = -1;
+	if(cantidadNumeros>0 && *resultadoMaximo != NULL && *resultadoMinimo != NULL)
+	{
+		for(i=0;i< cantidadNumeros;i++)
+		{
+			printf("ingrese un numero; \n");
+			scanf("%d",&numero);
+			if(i==0 || numero > maximo)
+			{
+				maximo=numero;
+			}
+			if(i==0 || numero < minimo)
+			{
+				minimo=numero;
+			}
+		}
+		*resultadoMaximo = maximo;
+		*resultadoMinimo = minimo;
+		retorno = 0;
+	}
+	return retorno;
+}
+
+
+
+
+
+
+
